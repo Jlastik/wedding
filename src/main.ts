f7dd8bdfbe523dd43d4474228d3ba05cd8e1e060
Jlastik <duh1296@mail.ru>
2024-05-22 21:24:51 +0300

    add reveal

--- src/main.ts
+++ src/main.ts
@@ -20,12 +20,39 @@ if (form) {
       .catch((e) => console.log(e));
   });
 }
+const isElementInViewport = (el: Element) => {
+  var rect = el.getBoundingClientRect();
 
-window.addEventListener("scroll", function () {
+  return (
+    rect.top >= 0 &&
+    rect.left >= 0 &&
+    rect.bottom <=
+      (window.innerHeight || document.documentElement.clientHeight) &&
+    rect.right <= (window.innerWidth || document.documentElement.clientWidth)
+  );
+};
+
+const checkForVisibility = () => {
+  var elements = document.querySelectorAll(".appearance");
+  elements.forEach((element) => {
+    if (isElementInViewport(element)) {
+      element.classList.add("show");
+    } else {
+      // element.classList.remove("show");
+    }
+  });
+};
+
+window.addEventListener("DOMContentLoaded", checkForVisibility, false);
+window.addEventListener("load", checkForVisibility, false);
+
+window.addEventListener("scroll", () => {
+  checkForVisibility();
+
   const distance = window.scrollY * 0.4;
 
   const ringGrape = document.querySelector("#ring-grape") as HTMLImageElement;
-  if (ringGrape && distance < 330 && distance > 100) {
+  if (ringGrape && distance < 330) {
     ringGrape.style.transform = `translateY(${distance}px)`;
   }
 });
